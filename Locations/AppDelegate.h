7819393dbb4ab4173792ef8f5a2ed096a32e0037
LeyLa Mehmed <leylamehmed@LeyLas-MacBook-Pro.local>
2015-10-04 14:22:18 +0300

    Initial Commit

---
+++ Locations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Locations
+//
+//  Created by LeyLa Mehmed on 10/4/15.
+//  Copyright (c) 2015 LeyLa Mehmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Locations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Locations
+//
+//  Created by LeyLa Mehmed on 10/4/15.
+//  Copyright (c) 2015 LeyLa Mehmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
